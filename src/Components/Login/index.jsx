1ed53e186a58efd6d9abc52d139bede82df9c28c
Felipe <felipe.m.c.b.fm@gmail.com>
2024-07-03 15:12:55 -0300

    modularização dos componentes input e buttons

--- src/Components/Login/index.jsx
+++ src/Components/Login/index.jsx
@@ -2,11 +2,12 @@ import styled from "styled-components"
 import { Link, useNavigate } from "react-router-dom"
 import { useState, useContext } from "react"
 import axios from "axios"
+import { PulseLoader } from "react-spinners"
 
-import { manipuladorDeInput } from "../../utils/ferramentas"
+import { renderInputs, renderButton } from "../../utils/ferramentas"
 import { Contexto } from "../../Contexto"
 
-  
+
 // TODO: DESATIVAR BOTÃO E INPUTS AO EFETUAR UMA REQUISIÇÃO E ADICIONAR EFEITO DE CARREGAMENTO
 
 /**
@@ -14,42 +15,24 @@ import { Contexto } from "../../Contexto"
  */
 export default function Login({ }) {
     // estados
-    const [loginInputs, setLoginInputs] = useState({ "E-mail": "", "Senha": ""});
-    const [mensagemDeErro, setMensagemDeErro] = useState({'ativa': false, 'erro': ""});
+    const [loginInputs, setLoginInputs] = useState({ "E-mail": "", "Senha": "" });
+    const [mensagemDeErro, setMensagemDeErro] = useState({ 'ativa': false, 'erro': "" });
     const [aguardandoRequisicao, setAguardandoRequisicao] = useState(false);
     // vars
     const indicesTipos = ['email', 'password'];
     const minimosRequeridos = ["7", "6"];
     const navigate = useNavigate();
-    
-    // variáveis globais
-    const {setDadosUsuario, setTokenUsuario} = useContext(Contexto);
 
-    /**
-     * renderização dos inputs de forma automatica
-     * @param {string} titulo - Título do objeto, usado como placeholder 
-     * @param {int} indice - contador do map
-     * */ 
-    function renderInputs(titulo, indice){
-        return(
-            <input
-                    key={titulo}
-                    type={indicesTipos[indice]}
-                    placeholder={titulo}
-                    required
-                    value={loginInputs[titulo]}
-                    minLength={minimosRequeridos[indice]}
-                    onChange={(evento) => manipuladorDeInput(evento, loginInputs, titulo, setLoginInputs)}
-                    disabled={aguardandoRequisicao}
-            />
-        );
-    };
+    // variáveis globais
+    const { setDadosUsuario, setTokenUsuario } = useContext(Contexto);
+
+    // function renderButton(texto, ativo)
 
     /**
      * Realiza a requisição de login para o servidor
      * @returns - recebe os dados do usuário e o token de acesso
      */
-    async function executarRequisicao(){
+    async function executarRequisicao() {
         try {
             // desativando inputs
             setAguardandoRequisicao(true)
@@ -64,7 +47,7 @@ export default function Login({ }) {
             setAguardandoRequisicao(false)
             // controle de erros
             const mensagemDeErro = erro?.response?.data
-            setMensagemDeErro({"ativa": true, "erro": mensagemDeErro || erro})
+            setMensagemDeErro({ "ativa": true, "erro": mensagemDeErro || erro })
             console.log("Erro ao efetuar login: ", erro?.response || erro)
         }
     };
@@ -73,41 +56,42 @@ export default function Login({ }) {
      * Atualiza o estado dadosDoUsuário e tokenDoUsuário se a requisição for recebida com sucesso
      * @param {Object} evento - Objeto que recebemos automaticamente
      */
-    async function subimissao(evento){
+    async function subimissao(evento) {
         // evitando dos dados serem perdidos 
         evento.preventDefault()
 
-        try{
+        try {
             // efetuando requisição para o servidor
             const dados = await executarRequisicao()
             const informacoesUsuario = dados?.data
 
             // validando as informações e atualizando estado
-            if(informacoesUsuario !== undefined){
+            if (informacoesUsuario !== undefined) {
                 // salvando token
                 setTokenUsuario(`Bearer ${informacoesUsuario.token}`)
                 delete informacoesUsuario.token
                 // salvando dados
                 setDadosUsuario(informacoesUsuario)
                 // desativando mensagem de erro, se existir
-                setMensagemDeErro({...mensagemDeErro, ativa:false})
+                setMensagemDeErro({ ...mensagemDeErro, ativa: false })
                 // Enviando usuário para a tela principal
-                // navigate("/home")
+                navigate("/home")
 
             }
-        }catch(e){
+        } catch (e) {
             // controle de erros
             console.log("Erro ao efetuar subimissão: ", e)
         }
     }
 
     return (
-        <TelaLogin onSubmit={async (e) =>{await subimissao(e)}}>
+        <TelaLogin onSubmit={async (e) => { await subimissao(e) }}>
             <h1>MyWallet</h1>
-            {Object.keys(loginInputs).map((titulo, indice) => renderInputs(titulo, indice))}
-            <button type="submit" disabled={aguardandoRequisicao}>Entrar</button>
-            {mensagemDeErro.ativa?<p>{mensagemDeErro["erro"]}</p>:<></>}
+            {Object.keys(loginInputs).map((titulo, indice) => (renderInputs(titulo, indicesTipos[indice], minimosRequeridos[indice], aguardandoRequisicao, loginInputs, setLoginInputs)))}
+            {renderButton("submit", "Entrar", aguardandoRequisicao, ()=>{})}
+            {mensagemDeErro.ativa ? <p>{mensagemDeErro["erro"]}</p> : <></>}
             <Link to="/cadastro">Primeira vez? Cadastre-se</Link>
+
         </TelaLogin>
     )
 }
--- src/utils/ferramentas.jsx
+++ src/utils/ferramentas.jsx
@@ -1,3 +1,5 @@
+import { PulseLoader } from "react-spinners";
+
 /**
  * Manipula os inputs usando os estados
  * @param {Object} evento - Objeto recebido por padrão do input 
@@ -5,9 +7,74 @@
  * @param {string} chave - Chave do objeto que está sendo manipulado
  * @param {Function} setEstado - Manipula o valor do input
  */
-
-
-export function manipuladorDeInput(evento, estado, chave, setEstado){
+function manipuladorDeInput(evento, estado, chave, setEstado){
         let copiaEstado = {...estado}
         setEstado({...copiaEstado, [chave]: evento.currentTarget.value})
+}
+
+// /**
+//      * renderização dos inputs de forma automatica
+//      * @param {string} titulo - Título do objeto, usado como placeholder 
+//      * @param {int} indice - contador do map
+//      * @param {boolean} ativo - define se o input estará ativo ou não
+//      * @param {Object} estado - estado que controla o input
+//      * @param {string} minimoRequerido -  que contenha o valor minimo de caracteres que os inputs devem ter, controlados pelo index do map
+//      * @param {Object} manipuladorEstado - setEstado, manipulador do estado
+//      * */
+// export function renderInputs(titulo, indice, ativo, estado, minimoRequerido, manipuladorEstado) {
+//     return (
+//         <input
+//             key={titulo}
+//             type={estado[indice]}
+//             placeholder={titulo}
+//             required
+//             value={estado[titulo]}
+//             minLength={minimoRequerido[indice]}
+//             onChange={(evento) => manipuladorDeInput(evento, estado, titulo, manipuladorEstado)}
+//             disabled={ativo}
+//         />
+//         );
+// };
+
+
+/**
+ * 
+ * @param {string} titulo - Nome que aparece no placeholder e referencia o elemento do estado que será manipulado
+ * @param {string} tipo - tipo de input: password, text, etc...
+ * @param {string} minimoRequerido - valor de caracteres mínimos
+ * @param {Object} estado - objeto de estados que será manipulado
+ * @param {Object} manipuladorEstado - setEstado, manipulador do estado
+ * @returns 
+ */
+export function renderInputs(titulo, tipo, minimoRequerido, isAtivo, estado, manipuladorEstado) {
+        return (
+            <input
+                key={titulo}
+                type={tipo}
+                placeholder={titulo}
+                required
+                value={estado[titulo]}
+                minLength={minimoRequerido}
+                onChange={(evento) => manipuladorDeInput(evento, estado, titulo, manipuladorEstado)}
+                disabled={isAtivo}
+            />
+            );
+    };
+
+
+/**
+ * @param {string} tipo - tipo do botão: subimit, etc..
+ * @param {string} texto - Texto do botao
+ * @param {boolean} ativo - ativo ou inativo
+ * @param {function} funcao - funcao para o onclick
+        
+ }}} funcao 
+ * @returns 
+ */
+export function renderButton(tipo, texto, ativo, funcao){        
+    return(
+        <button type={tipo} disabled={ativo} onClick={funcao}>
+                {ativo?<PulseLoader color="#ffffff" size={10}/> : texto}
+        </button>
+        );
 }
